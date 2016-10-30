e65180c96188e47477d3a363f7766c2cd1738dce
lcluber <ludoclub@hotmail.com>
2016-10-30 22:46:47 +0100

    fixed licence

--- src/orbis.js
+++ src/orbis.js
@@ -10,6 +10,8 @@ var ORBIS = {
   * @copyright (c) 2011 Ludovic Cluber
 
   * @license
+  * MIT License
+  *
   * Permission is hereby granted, free of charge, to any person obtaining a copy
   * of this software and associated documentation files (the "Software"), to deal
   * in the Software without restriction, including without limitation the rights
