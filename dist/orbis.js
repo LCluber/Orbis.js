ef0ef2e27295a64434944bd77a8d25b653627003
lcluber <ludovic.cluber@gmail.com>
2017-01-10 23:07:53 +0100

    updated FrameRat.js dependency to version 0.2.2

--- dist/orbis.js
+++ dist/orbis.js
@@ -478,7 +478,7 @@ ORBIS.Request = {
 * http://frameratjs.lcluber.com
 */
 var FRAMERAT = {
-    revision: "0.2.1",
+    revision: "0.2.2",
     id: null,
     onAnimate: function() {},
     frameNumber: 0,
@@ -507,17 +507,28 @@ var FRAMERAT = {
         if (this.fsm.play()) {
             this.clock.start();
             this.requestNewFrame(scope);
+            return true;
         }
-        return this.fsm.getStatus();
+        return false;
     },
     pause: function() {
-        if (this.fsm.pause()) this.cancelAnimation();
+        if (this.fsm.pause()) {
+            this.cancelAnimation();
+            return true;
+        }
+        return false;
+    },
+    toggle: function() {
+        if (!this.play()) this.pause();
         return this.fsm.getStatus();
     },
     stop: function() {
-        this.pause();
-        this.clock.init();
-        this.frameNumber = 0;
+        if (this.pause()) {
+            this.clock.init();
+            this.frameNumber = 0;
+            return true;
+        }
+        return false;
     },
     getTotalTime: function(decimals) {
         return this.clock.getTotal(decimals);
