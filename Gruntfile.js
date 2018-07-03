3efc00135253a78fd142a34ab6d2838308da6651
Estelle Picq <est.picq@gmail.com>
2018-07-03 16:52:59 +0200

    Temporary commit with typedoc tasj

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt){
   var publicDir       = webDir + 'public/';
   var nodeDir         = 'node_modules/';
   var bowerDir        = 'bower_components/';
+  var docDir          = 'doc/';
 
 
   var banner    = '/** MIT License\n' +
@@ -53,6 +54,10 @@ module.exports = function(grunt){
                 compiledSrcDir + '*'
               ]
       },
+      doc:{
+        src: [  docDir + '*'
+              ]
+      },
       websass:{
         src: [  webDir + 'sass/build/*',
                 publicDir + 'css/*'
@@ -67,6 +72,16 @@ module.exports = function(grunt){
         ]
       }
     },
+    typedoc: {
+  		build: {
+  			options: {
+  				out: docDir,
+  				target: 'es6',
+          name: projectName + '.js - Documentation'
+  			},
+  			src: [srcDir + 'ts/*.ts']
+  		}
+  	},
     jshint: {
       options: {
         jshintrc: 'config/.jshintrc'
@@ -445,6 +460,7 @@ module.exports = function(grunt){
   grunt.loadNpmTasks( 'grunt-tslint' );
   grunt.loadNpmTasks( 'grunt-ts' );
   grunt.loadNpmTasks( 'grunt-rollup' );
+  grunt.loadNpmTasks( 'grunt-typedoc' );
 
   grunt.registerTask( 'lib',
                       'build the library in the dist/ folder',
@@ -459,6 +475,12 @@ module.exports = function(grunt){
                       ]
                     );
 
+  grunt.registerTask( 'doc',
+                      'Compile lib documentation',
+                      [ 'clean:doc',
+                        'typedoc'
+                      ]
+                    );
 
   grunt.registerTask( 'serve',
                       'launch server, open website and watch for changes',
@@ -510,6 +532,8 @@ module.exports = function(grunt){
                         grunt.task.run('lib');
                         //build site
                         grunt.task.run('website');
+                        //build documentation
+                        grunt.task.run('doc');
                       }
                     );
 
